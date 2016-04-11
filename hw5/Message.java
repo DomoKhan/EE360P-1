4b04ec014a25c80579770eb2c73b3e22e9f2b676
Domokhan <rtt1636@utexas.edu>
2016-04-11 11:00:09 -0500

    Made it more multi-threaded and made massive changes to how the code looks.

---
+++ hw5/Message.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Message {
+	int timestamp;
+	boolean changed;
+	ArrayList<String> supplies;
+    ArrayList<Integer> quantities;
+    boolean okay;
+    int id;
+    boolean wantCS;
+    Message(int timestamp, ArrayList<String> supplies, ArrayList<Integer> quantities, int id){
+    	this.timestamp = timestamp;
+    	this.supplies = supplies;
+    	this.quantities = quantities;
+    	this.id = id;
+    	changed = false;
+    	okay = false;
+    }
+    
+    Message(boolean okay, ArrayList<Integer> quantities){
+    	this.okay = okay;
+    	timestamp = Integer.MAX_VALUE;
+    	this.quantities = quantities;
+    }
+    
+    public boolean getOkay(){
+    	return okay;
+    }
+    
+    public ArrayList<Integer> getQuantities(){
+    	return quantities;
+    }
+    
+    public int getID(){
+    	return id;
+    }
+    
+    public int getTime(){
+    	return timestamp;
+    }
+    
+    
+    
+    
+}
--- hw5/Server.java
+++ hw5/Server.java
@@ -3,6 +3,8 @@ import java.io.DataOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,10 +12,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 public class Server {
 	final static String baseName = "HW5";
+	final static boolean printStatements = true;
 	static ArrayList<String> ips;
 	static ArrayList<Integer> ports;
+	static ArrayList<Integer> serverPorts; // only used for each server sending messages to each other
 	static ArrayList<String> supplies;
     static ArrayList<Integer> quantities;
     static ArrayList<Integer> orderIDs = new ArrayList<Integer>();
@@ -21,73 +26,148 @@ public class Server {
     static ArrayList<String> myProductName = new ArrayList<String>();
     static ArrayList<String> myOrder = new ArrayList<String>();
     static AtomicInteger orderIDinit;
-    
+    static DirectClock clock;
+    static int myID;
+    static int[] req_queue;
+    static ArrayList<String> client_request;
     public static void main (String[] args) {
-
 	    Scanner sc = new Scanner(System.in);
-	    int myID = sc.nextInt();
+	    myID = sc.nextInt();
 	    int numServer = sc.nextInt();
 	    String inventoryPath = sc.next();
 	    ips = new ArrayList<String>();
 	    ports = new ArrayList<Integer>();
+	    serverPorts = new ArrayList<Integer>();
+	    client_request = new ArrayList<String>();
+	    req_queue = new int[numServer];
+	    for(int i = 0; i < numServer; i++) // means don't want the CS 
+	    	req_queue[i] = Integer.MAX_VALUE;
+	    
 	    assert myID < numServer;
 	    for (int i = 0; i < numServer; i++) {
 	      // TODO: parse inputs to get the ips and ports of servers
 	      String ip_port = sc.next();
 	      String[] nums = ip_port.split(":");
+	      
 	      assert nums.length == 2;
 	      ips.add((nums[0]));
 	      ports.add(Integer.parseInt(nums[1]));
 	    }
 	    
-	    
+	    // new ports for server-server connection
+	    { // putting this in inner brackets in case I reuse i
+	    	int i = 0;
+	    	int possiblePort = ports.get(i) + 1;
+	    	while(i < numServer){
+	    		if(ports.contains(possiblePort) || serverPorts.contains(possiblePort)){ // prevent reuse of ports
+	    			++possiblePort;
+	    		}
+	    		else{ // unused port - hopefully fine - tbh I don't really know if it is used by some other process
+	    			serverPorts.add(possiblePort);
+	    			++i;
+	    			if(i >= numServer)
+	    				break;
+	    			possiblePort = ports.get(i) + 1;
+	    		}
+	    	} 	
+	    }
 	    
 	    // TODO: start server socket to communicate with clients and other servers
 	    ServerSocket socket = null;
 	    try {
 	        int myPort = ports.get(myID - 1);
-                System.out.println(myPort);
+	    	System.out.println(myPort);
 			socket = new ServerSocket(myPort);
 			
 	    } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    parseInventory(inventoryPath, socket);
+	    parseInventory(inventoryPath);
 	    
-	    // create mutex
-	    String[] linkerInfo = {baseName, Integer.toString(myID), Integer.toString(numServer)};
-	    Linker link;
-	    LamportMutex mutex = null;
-		try {
-			link = new Linker(linkerInfo);
-			mutex = new LamportMutex(link);
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-	    
+	    if(printStatements)
+	   		System.out.println("Parsed Inventory");
 	    // TODO: handle request from client
-	    Socket connectionSocket;
-	    while(true){
-		    try{
-	    		connectionSocket = socket.accept();
-	    		//awaitOkay(connectionSocket, numServer);
-	    		
-	    		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		    	DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-		    	String s = new String(inFromClient.readLine());
-		    	String[] tokens = s.split(" ");
-		    	clientRequest(tokens, mutex, s, outToClient);
-		    	
-	    	}catch(IOException e){
-				//System.out.println("Could not");
+	    Thread serverListener = createServerRequestListener();
+	    if(printStatements)
+	    	System.out.println("Created Initial Server Listener");
+	    Thread clientListener = createClientRequestListener(socket);
+	    if(printStatements)
+	    	System.out.println("Starting Threads");
+	    serverListener.start();
+	    clientListener.start();
+	    if(printStatements)
+	    	System.out.println("Entering Loop");
+		while(true){
+		    // get client command - makes its own thread
+			// create listener for request 
+			if(!clientListener.isAlive()){
+				String request = client_request.remove(0); 
+				clientListener = createClientRequestListener(socket);
+				clientListener.start();
+				clientRequest(request, socket, numServer);			
+			}
+			
+			if(!serverListener.isAlive()){
+				serverListener = createServerRequestListener();	
+				serverListener.start();
+				sendOkays(numServer);
 			}
 	    }
 	  }
-  
+    
+    
+
+    
+   public static Thread createClientRequestListener(final ServerSocket socket){
+    	Thread t = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					Socket connectionSocket = null;
+					connectionSocket = socket.accept();
+					final BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+					String s = new String(inFromClient.readLine());
+					client_request.add(s);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		return t;
+    }
+    
+    // create listener for server -> server
+    // Listen for if a message comes to this server - listens for this ports
+    public static Thread createServerRequestListener(){
+ 	    Thread t = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					ServerSocket socket = null;
+			    	Socket tcpsocket;
+			    	int myPort = serverPorts.get(myID - 1);
+			    	
+		 			socket = new ServerSocket(myPort);
+		 			tcpsocket = socket.accept();
+		 			final ObjectInputStream ois = new ObjectInputStream(tcpsocket.getInputStream());
+					Message msg = (Message)ois.readObject();
+					req_queue[msg.getID()] = msg.getTime();
+					// maybe update inventory
+				} catch (ClassNotFoundException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		return t;
+    }
     
     // parse Inventory file
-    public static void parseInventory(String inventoryPath, ServerSocket socket){
+    public static void parseInventory(String inventoryPath){
     	
 	    supplies = new ArrayList<String>();
 	    quantities = new ArrayList<Integer>();
@@ -103,39 +183,141 @@ public class Server {
 				quantities.add(Integer.parseInt(tokens[1]));
 			}
 	    	br.close();
-	    	socket.close();
 	    } catch (NumberFormatException | IOException e1) {
 	    	// TODO Auto-generated catch block
 	    	e1.printStackTrace();
 	    }	
     }
     
+    public static void sendMsg(int id, Message req) throws IOException{
+    	Socket tcpSocket = new Socket(InetAddress.getByName(ips.get(id)), serverPorts.get(id));
+	    ObjectOutputStream oos = new ObjectOutputStream(tcpSocket.getOutputStream());
+	    try {
+			oos.writeObject(req);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    tcpSocket.close();
+    }
     
-    // send a message to all servers 
-	// wait for acceptance from all
-    public static void awaitOkay(Socket connectionSocket, int numServer){
-    	try {
-            Socket tcpSocket;
-            DataOutputStream outToServer;
-            BufferedReader inFromServer;
-    		for(int i = 0; i < numServer; i++){
-    			
-    			tcpSocket = new Socket(InetAddress.getByName(ips.get(i)), ports.get(i));
-	    	    outToServer = new DataOutputStream(tcpSocket.getOutputStream());	
-	    	    inFromServer = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream())); 
-			
+    public static void sendOkays(int numServer){
+    	int myTime = req_queue[myID];
+    	for(int i = 0; i < numServer; ++i){
+    		if(i == myID)
+    			continue;
+    		if(req_queue[i] < myTime){
+    			boolean okay = true;
+    			Message msg = new Message(okay, quantities);
+    			try {
+					sendMsg(i, msg);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
     		}
+    	}
+    }
+    
+    // send requests to all the servers
+    public static void broadcastMsg(int numServer){
+    	try {
+            // broadcast message
+    		for(int i = 0; i < numServer; i++){
+    			if(i == myID)
+    				continue;
+    			Message req = new Message(req_queue[myID], supplies, quantities, myID);
+    			sendMsg(i, req);
+    		}
+    		// probably do it differently
     	} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
     
-    public static String clientRequest(String[] tokens, LamportMutex mutex, String order, DataOutputStream outToClient){
+    // check if you can access the CS
+    public static boolean okayCS(int numServer){
+		for(int i = 0; i < numServer; ++i){
+			if(req_queue[i] < clock.getValue(myID))
+				return false;
+			if(req_queue[i] == clock.getValue(myID) && i < myID)
+				return false;
+		}
+		return true;
+    }
+    
+    public static void listenForOkays(int numServer){
+    	for(int i = 0; i < numServer; ++i){
+    		final Socket tcpSocket;
+			try {
+				tcpSocket = new Socket(InetAddress.getByName(ips.get(i)), serverPorts.get(i));
+				final ObjectInputStream ois = new ObjectInputStream(tcpSocket.getInputStream());
+		    	new Thread(new Runnable(){
+					public void run() {
+						try {
+							Message msg = (Message)ois.readObject();
+							req_queue[msg.getID()] = msg.getTime();
+							// requestCS is used only for changes in inventory
+							quantities = msg.getQuantities();
+						} catch (ClassNotFoundException | IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}		
+		    	});
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}   
+    	}
+    }
+    
+    // send a message to all servers 
+	// wait for acceptance from all
+    public synchronized static void requestCS(int numServer){
+    	clock.tick();
+    	req_queue[myID] = clock.getValue(myID);
+    	broadcastMsg(numServer);
+    	listenForOkays(numServer);
+    	while(!okayCS(numServer)){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    public synchronized static void releaseCS(int numServer){
+    	req_queue[myID] = Integer.MAX_VALUE;
+    	broadcastMsg(numServer);
+    }
+    
+    public static String clientRequest(String order, ServerSocket socket, int numServer){
+    	// TODO: handle request from client
+    	if(printStatements)
+    		System.out.println("Dealing with Client Request");
+	    Socket connectionSocket = null;
+		try {
+			connectionSocket = socket.accept();
+	    } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	DataOutputStream outToClient = null;
+		try {
+			outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     	String rString = order;
+    	String[] tokens = order.split(" ");
     	Integer orderID = orderIDinit.get();
     	if(tokens[0].equals("purchase")){
-    		mutex.requestCS();
+    		requestCS(numServer);
     		
     		int indexSupplies = supplies.indexOf(tokens[2]);
     		if(indexSupplies < 0){
@@ -162,11 +344,11 @@ public class Server {
     		}
     		try {
 				outToClient.writeBytes(rString + '\n');
+				releaseCS(numServer);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    		mutex.releaseCS();
     	}
     	else if(tokens[0].equals("cancel")){
 			//checks if the orderID is present
@@ -179,7 +361,6 @@ public class Server {
 			
 			//else remove order from all of the lists...lol
 			else {
-				mutex.requestCS();
 				rString = "Order " + tokens[1] + " is canceled";
 				int supplyIndex = supplies.indexOf(myProductName.get(indexID));
 				quantities.set(supplyIndex, Integer.parseInt(myOrder.get(indexID)) + quantities.get(supplyIndex));
@@ -187,7 +368,6 @@ public class Server {
 			    userName.remove(indexID);
 			    myProductName.remove(indexID);
 			    myOrder.remove(indexID);
-			    mutex.releaseCS();
 				
 			}
 			//System.out.println("Sending data");
