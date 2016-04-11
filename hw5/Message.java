677582d772da344174ffc20fa3e655e5f8a7c530
Domokhan <rtt1636@utexas.edu>
2016-04-11 16:21:12 -0500

    Made it serializable

--- hw5/Message.java
+++ hw5/Message.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-
-public class Message {
+import java.io.Serializable;
+public class Message implements Serializable{
 	int timestamp;
 	boolean changed;
 	ArrayList<String> supplies;
